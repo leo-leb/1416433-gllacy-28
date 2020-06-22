e02592bea710e363bad08a9ea949f3b09d03a85d
leo-leb <swan.leon.id@gmail.com>
2020-06-22 12:48:49 +0300

    9.16-adding-animating-map-v1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+// FEEDBACK-FORM
+
 var feedback = document.querySelector(".feedback-overlay");
 var feedback__open = document.querySelector(".button--fb");
-var feedback__close = document.querySelector(".feedback__button-cross");
-var feedback__form = feedback.querySelector(".feedback__popup");
-var firstname = feedback.querySelector("[name=firstname]");
-var email = feedback.querySelector("[name=email]");
+var feedback__form = document.querySelector(".feedback__popup");
+var feedback__close = feedback__form.querySelector(".feedback__button-cross");
+var firstname = feedback__form.querySelector("[name=firstname]");
+var email = feedback__form.querySelector("[name=email]");
 var storage_firstname = localStorage.getItem("firstname");
 var storage_email = localStorage.getItem("email");
 
@@ -23,6 +25,15 @@ feedback__open.addEventListener("click", function (evt) {
 feedback__close.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedback.classList.remove("show");
+  feedback__form.classList.remove("modal-show");
+  feedback__form.offsetWidth = feedback__form.offsetWidth;
+  feedback__form.classList.remove("modal-error");
+});
+
+feedback.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedback.classList.remove("show");
+  feedback__form.classList.remove("modal-show");
   feedback__form.offsetWidth = feedback__form.offsetWidth;
   feedback__form.classList.remove("modal-error");
 });
@@ -35,4 +46,74 @@ feedback__form.addEventListener("submit", function (evt) {
     localStorage.setItem("firstname", firstname.value);
     localStorage.setItem("email", email.value);
   }
+});
+
+// GOOGLE-MAP
+
+function initMap() {
+  var pos = { lat: 59.938829, lng: 30.323069 };
+  var opt = {
+    center: pos,
+    zoom: 17,
+  };
+
+  var myMap = new google.maps.Map(document.getElementById("map"), opt);
+
+  var image = "/img/icons/ice-pin.png";
+
+  var marker = new google.maps.Marker({
+    position: pos,
+    map: myMap,
+    icon: image,
+  });
+}
+
+// SLIDER
+
+var slider_menu = document.querySelector(".index-content__general-offer");
+var body_wrapper = document.querySelector("body");
+var slide_1 = slider_menu.querySelector(".slide-first");
+var slide_2 = slider_menu.querySelector(".slide-second");
+var slide_3 = slider_menu.querySelector(".slide-third");
+var dot_1 = slider_menu.querySelector(".dot-first");
+var dot_2 = slider_menu.querySelector(".dot-second");
+var dot_3 = slider_menu.querySelector(".dot-third");
+
+dot_1.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  slide_1.classList.add("current");
+  slide_2.classList.remove("current");
+  slide_3.classList.remove("current");
+  body_wrapper.classList.add("body__wrapper-green");
+  body_wrapper.classList.remove("body__wrapper-blue-gray");
+  body_wrapper.classList.remove("body__wrapper-brown-gray");
+  dot_1.classList.add("current");
+  dot_2.classList.remove("current");
+  dot_3.classList.remove("current");
+});
+
+dot_2.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  slide_1.classList.remove("current");
+  slide_2.classList.add("current");
+  slide_3.classList.remove("current");
+  body_wrapper.classList.remove("body__wrapper-green");
+  body_wrapper.classList.add("body__wrapper-blue-gray");
+  body_wrapper.classList.remove("body__wrapper-brown-gray");
+  dot_1.classList.remove("current");
+  dot_2.classList.add("current");
+  dot_3.classList.remove("current");
+});
+
+dot_3.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  slide_1.classList.remove("current");
+  slide_2.classList.remove("current");
+  slide_3.classList.add("current");
+  body_wrapper.classList.remove("body__wrapper-green");
+  body_wrapper.classList.remove("body__wrapper-blue-gray");
+  body_wrapper.classList.add("body__wrapper-brown-gray");
+  dot_1.classList.remove("current");
+  dot_2.classList.remove("current");
+  dot_3.classList.add("current");
 });
